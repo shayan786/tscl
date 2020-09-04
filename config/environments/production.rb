ecea3c693c4b6f5d80a1d234313da84bd40857bd
Shy <shayan.dhanani@appydnamics.com>
2020-09-04 15:31:35 -0500

    mailer test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "alki_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@tscl.us'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
